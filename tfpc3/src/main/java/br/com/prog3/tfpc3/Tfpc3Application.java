71613166f63cc4e937c445a4a5da7583b9e15ad0
emllmnn <8692workme@gmail.com>
2023-12-10 23:35:05 -0300

    tfpc3

---
+++ tfpc3/src/main/java/br/com/prog3/tfpc3/Tfpc3Application.java
@@ -0,0 +1,13 @@
+package br.com.prog3.tfpc3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tfpc3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tfpc3Application.class, args);
+	}
+
+}
---
+++ tfpc3/src/test/java/br/com/prog3/tfpc3/Tfpc3ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.prog3.tfpc3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tfpc3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
